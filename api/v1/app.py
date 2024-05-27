2757c40ac9d49e70d1abe624232f3d68d5d77a5c
Sarah-Gad <sarahgad298@gmail.com>
2024-05-27 22:03:38 +0300

    edit

--- api/v1/app.py
+++ api/v1/app.py
@@ -10,6 +10,7 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 allow = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
+
 @app.teardown_appcontext
 def teardown_appcontext(exception):
     """This is the methos to call close function"""
@@ -17,4 +18,6 @@ def teardown_appcontext(exception):
 
 
 if __name__ == "__main__":
-    app.run(host=getenv("HBNB_API_HOST", "0.0.0.0"), port=int(getenv("HBNB_API_PORT", "5000")))
+    app.run(
+        host=getenv("HBNB_API_HOST", "0.0.0.0"),
+        port=int(getenv("HBNB_API_PORT", "5000")))
