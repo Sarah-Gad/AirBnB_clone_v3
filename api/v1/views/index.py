e84dba20d3196de2c888949cd4b92c5d920d4461
Sarah-Gad <sarahgad298@gmail.com>
2024-05-27 22:12:10 +0300

    edited index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,9 +4,7 @@
 from api.v1.views import app_views
 from flask import jsonify
 
-app_views.route("/status", methods=['GET'], strict_slashes=False)
-
-
+@app_views.route("/status", methods=['GET'], strict_slashes=False)
 def getsatus():
     """This method will be called when the user access this url"""
     return jsonify({"status": "OK"})
