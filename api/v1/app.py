f3c68a0abcc3ef95de6225a6d94cc145cf5c1e6f
Sarah-Gad <sarahgad298@gmail.com>
2024-05-27 22:02:36 +0300

    edit

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,16 +4,17 @@ from os import getenv
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
-
+allow = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 @app.teardown_appcontext
-def teardown_appcontext(excepy):
+def teardown_appcontext(exception):
     """This is the methos to call close function"""
     storage.close()
 
 
 if __name__ == "__main__":
-    app.run(getenv("HBNB_API_HOST"), getenv("HBNB_API_PORT"))
+    app.run(host=getenv("HBNB_API_HOST", "0.0.0.0"), port=int(getenv("HBNB_API_PORT", "5000")))
