2a8f4bbc17d1f778f72d84b06c230649ebd81186
Sarah-Gad <sarahgad298@gmail.com>
2024-05-28 00:40:49 +0300

    complete dtask nn.4

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,7 +4,22 @@
 from api.v1.views import app_views
 from flask import jsonify
 
+
 @app_views.route("/status", methods=['GET'], strict_slashes=False)
 def getsatus():
     """This method will be called when the user access this url"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route("/stats", methods=['GET'], strict_slashes=False)
+def getnumber():
+    """This method will be called when the the user aaccess this url"""
+    allinstances = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
+    }
+    return jsonify(allinstances)
